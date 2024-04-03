5c994a586cbf3fd2ff1319fdfdd7c401915b8d38
linda-20 <lindaaron40@gmail.com>
2024-04-03 22:10:14 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -22,6 +22,40 @@ def index():
 
 # Add views here
 
+@app.route('/earthquakes/<int:id>')
+def earthquakes_get_by_id(id):
+    earthquake = db.session.get(Earthquake, id)
+    if earthquake:
+        body = {
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+        }
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+
+    return make_response(body, status)
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_filter_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    count = len(earthquakes)
+    body = [{
+        'id': earthquake.id,
+        'location': earthquake.location,
+        'magnitude': earthquake.magnitude,
+        'year': earthquake.year
+    } for earthquake in earthquakes]
+
+    response = {'count': count, 'quakes': body}
+    status = 200
+
+    return make_response(response, status)
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
